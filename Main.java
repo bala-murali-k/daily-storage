6816d603e79f5703415f4119bde64faf1d032332
BalaMurali0701 <k.balamurali0701@gmail.com>
2024-07-10 11:32:12 -0700

    Check the termux is clearing or not

--- Main.java
+++ Main.java
@@ -1,6 +1,19 @@
+import java.io.IOException;
+
 class Main {
     public static void main(String[] args) {
         String osname = System.getProperty("os.name");
+        clearScreen(osname);
         System.out.println(osname);
+    }
+
+    public static void clearScreen(String sytem_name) {
+        try {
+            if(System.getProperty("os.name").contains("Linux")) {
+                new ProcessBuilder("clear").inheritIO().start().waitFor();
+            }
+        } catch (IOException | InterruptedException e) {
+                e.printStackTrace();
+        }
     }
 }
